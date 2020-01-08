bf8e7d4da52de346c1fd0fcf63834961edcf75ae
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-01-08 09:12:11 -0300

    Showing Different Content Based on Device Orientation

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,30 +5,32 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  final Function deleteTransaction;
+  final Function deleteTx;
 
-  TransactionList(this.transactions, this.deleteTransaction);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  )),
-            ],
-          )
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
@@ -57,9 +59,7 @@ class TransactionList extends StatelessWidget {
                   trailing: IconButton(
                     icon: Icon(Icons.delete),
                     color: Theme.of(context).errorColor,
-                    onPressed: () {
-                      deleteTransaction(transactions[index].id);
-                    },
+                    onPressed: () => deleteTx(transactions[index].id),
                   ),
                 ),
               );
